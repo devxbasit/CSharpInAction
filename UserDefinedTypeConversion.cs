7a2d9bbc408a3e0a3cdf94d445d65373dc986b34
Basit Shafi <mailtobasit74@gmail.com>
2021-11-01 16:01:56 +0530

    Added User Defined Type Conversion Example

--- UserDefinedTypeConversion.cs
+++ UserDefinedTypeConversion.cs
@@ -1,42 +1,56 @@
 using System;
 
-class UserDefinedTypeConversion{
-    
-class Country{
-    public long CountryId { get; set; }
-    public string Name { get; set; }
+class UserDefinedTypeConversion
+{
 
-}
-
-class CountryDto {
-    public long CountryIdentityCode { get; set; }
-    public string CountryName { get; set; }
-
-    public static implicit operator Country(Country country){
-        return new CountryDto(){
-            CountryIdentityCode = country.CountryId,
-            CountryName = country.Name
-        };
+    class Country
+    {
+        public long CountryId { get; set; }
+        public string Name { get; set; }
+        public Country(long countryId, string name)
+        {
+            CountryId = countryId;
+            Name = name;
+        }
     }
-    
-    public static implicit operator CountryDto(CountryDto countryDto){
-        return new Country(){
-            CountryId = countryDto.CountryIdentityCode,
-            Name = countryDto.CountryName
-        };
+
+    class CountryDto
+    {
+        public long CountryIdentityCode { get; set; }
+        public string CountryName { get; set; }
+
+        public CountryDto(long countryIdentityCode, string countryName)
+        {
+            CountryIdentityCode = countryIdentityCode;
+            CountryName = countryName;
+        }
+
+
+        // Dto to Entity  - Overloading Country
+        public static implicit operator Country(CountryDto countryDto)
+        {
+            return new Country(countryDto.CountryIdentityCode, countryDto.CountryName);
+        }
+
+
+        // Entity to Dto - Overloading CountryDto
+        public static implicit operator CountryDto(Country country)
+        {
+            return new CountryDto(country.CountryId, country.Name);
+        }
     }
-}
 
-    public static void Main(string[] args){
+    public static void Main(string[] args)
+    {
 
-    //     Country country = new Country(){
-    //         CountryId = 1,
-    //         Name = "India"
-    //     };
+        Country country = new Country(1, "USA");
 
-    //    // CountryDto countryDto = (CountryDto)country;
+        CountryDto countryDto = (CountryDto)country;
+        Console.WriteLine(countryDto.CountryName);
 
-    //     Console.WriteLine(countryDto.ToString());
+        countryDto.CountryName = "Tokoyo";
+        country = (Country)countryDto;
 
+        Console.WriteLine(country.Name);
     }
 }
