f5269573738a72fb1f7c2f067bd0442548378b30
Basit Shafi <mailtobasit74@gmail.com>
2022-01-15 20:51:37 +0530

    LINQ in Action

--- Chapter_10_Lambda_LINQ_FP/LINQ_GroupBy.cs
+++ Chapter_10_Lambda_LINQ_FP/LINQ_GroupBy.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Linq;
 
 class LINQ_GroupBy
 {
     public static void Main()
     {
+        var text = @"Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.";
+        
+        var groups = text.Split(new char[] {' ', ',', '.'})
+                     .GroupBy(w => w.Length, w => w.ToLower())
+                     .Select(g => new {Length = g.Key, Words = g})
+                     .Where(g => g.Length > 0)
+                     .OrderBy(g => g.Length);
 
+        foreach (var group in groups)
+        {
+            Console.WriteLine($"Length = {group.Length}");
+
+            foreach (var word in group.Words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+        }      
     }
 }
