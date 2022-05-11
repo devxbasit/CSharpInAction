126e581a04ea92de4a7660b49515de2fd8d3afc0
devxbasit <mailtobasit74@gmail.com>
2022-05-11 23:52:48 +0530

    User-defined conversion

--- Chapter_15_NewFeaturesOfC#8/InterfaceDefaultImplementation.cs
+++ Chapter_15_NewFeaturesOfC#8/InterfaceDefaultImplementation.cs
@@ -10,7 +10,7 @@ class InterfaceDefaultImplementation
 
         // error - Add, and Sub does not belong to Calc class, (Fix - use interface variable)
         // c.Add(1, 2);
-        // c.Sub(1, 2);
+        // c.Sub(1, 2+);
 
         ICalc ic = new Calc();
         ic.Add(1, 2);
--- Chapter_4_VariousUserDefinedTypes/UserDefinedTypeConversion.cs
+++ Chapter_4_VariousUserDefinedTypes/UserDefinedTypeConversion.cs
@@ -2,56 +2,11 @@ using System;
 
 class UserDefinedTypeConversion
 {
-
-    class Country
-    {
-        public long CountryId { get; set; }
-        public string Name { get; set; }
-        public Country(long countryId, string name)
-        {
-            CountryId = countryId;
-            Name = name;
-        }
-    }
-
-    class CountryDto
-    {
-        public long CountryIdentityCode { get; set; }
-        public string CountryName { get; set; }
-
-        public CountryDto(long countryIdentityCode, string countryName)
-        {
-            CountryIdentityCode = countryIdentityCode;
-            CountryName = countryName;
-        }
-
-
-        // Dto to Entity  
-        public static implicit operator Country(CountryDto countryDto)
-        {
-            return new Country(countryDto.CountryIdentityCode, countryDto.CountryName);
-        }
-
-
-        // Entity to Dto 
-        public static implicit operator CountryDto(Country country)
-        {
-            return new CountryDto(country.CountryId, country.Name);
-        }
-
-        /*
-        //https://www.infoworld.com/article/3606436/how-to-use-implicit-and-explicit-operators-in-csharp.html
-        Note you cannot have both implicit and explicit operators defined in a class.
-        If you have defined an implicit operator, you will be able to convert objects both implicitly and
-        explicitly. However, if you have defined an explicit operator, you will be able to convert objects 
-        explicitly only. This explains why you cannot have both implicit and explicit operators in a class. 
-        Although an implicit cast is more convenient to use, an explicit cast provides better clarity and readability 
-        of your code.
-        */
-    }
-
     public static void Main(string[] args)
     {
+
+        // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/user-defined-conversion-operator
+
         Country country = new Country(1, "USA");
 
         CountryDto countryDto = (CountryDto)country;
@@ -62,5 +17,92 @@ class UserDefinedTypeConversion
         country = (Country)countryDto;
 
         Console.WriteLine(country.Name);
+
+
+        Digit d1 = new Digit(5);
+
+        // implicit
+        byte b = d1;
+
+
+        // explicit from byte to digit
+        Digit d2 = (Digit)b;
+
+        Console.WriteLine($"d1 = {d1}");
+        Console.WriteLine($"b = {b}");
+        Console.WriteLine($"d2 = {d2}");
+
+
     }
+}
+
+
+class Digit
+{
+    private readonly byte _digit;
+
+    public Digit(byte digit)
+    {
+        if (digit > 9)
+        {
+            throw new ArgumentOutOfRangeException(nameof(digit), "Digit cannnot be greater than nine.");
+        }
+
+        _digit = digit;
+    }
+
+    public static implicit operator byte(Digit d) => d._digit;
+    public static explicit operator Digit(byte b) => new Digit(b);
+
+
+    public override string ToString() => $"{_digit}";
+
+}
+
+
+class Country
+{
+    public long CountryId { get; set; }
+    public string Name { get; set; }
+    public Country(long countryId, string name)
+    {
+        CountryId = countryId;
+        Name = name;
+    }
+}
+
+class CountryDto
+{
+    public long CountryIdentityCode { get; set; }
+    public string CountryName { get; set; }
+
+    public CountryDto(long countryIdentityCode, string countryName)
+    {
+        CountryIdentityCode = countryIdentityCode;
+        CountryName = countryName;
+    }
+
+
+    // Dto to Entity  
+    public static implicit operator Country(CountryDto countryDto)
+    {
+        return new Country(countryDto.CountryIdentityCode, countryDto.CountryName);
+    }
+
+
+    // Entity to Dto 
+    public static implicit operator CountryDto(Country country)
+    {
+        return new CountryDto(country.CountryId, country.Name);
+    }
+
+    /*
+    //https://www.infoworld.com/article/3606436/how-to-use-implicit-and-explicit-operators-in-csharp.html
+    Note you cannot have both implicit and explicit operators defined in a class.
+    If you have defined an implicit operator, you will be able to convert objects both implicitly and
+    explicitly. However, if you have defined an explicit operator, you will be able to convert objects 
+    explicitly only. This explains why you cannot have both implicit and explicit operators in a class. 
+    Although an implicit cast is more convenient to use, an explicit cast provides better clarity and readability 
+    of your code.
+    */
 }
