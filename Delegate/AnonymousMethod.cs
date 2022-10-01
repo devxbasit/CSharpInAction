3a1586bc3de6545e4bf72a60b1512be13a765c35
basit <devxbasit@gmail.com>
2022-10-01 23:21:24 +0530

    structure fix

---
+++ Delegate/AnonymousMethod.cs
@@ -0,0 +1,16 @@
+using System;
+
+class AnonymousMethod{
+    public static void Main() {
+        
+        int i = 10;
+
+        print fn = delegate()
+
+
+    
+
+    }
+    
+    public void Print(int val);
+}
