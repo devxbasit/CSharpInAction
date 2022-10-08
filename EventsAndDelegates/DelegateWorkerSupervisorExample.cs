db73ef728e8c9b16ad487c454d4d27aaee7647c9
basit <devxbasit@gmail.com>
2022-10-08 20:33:29 +0530

    Func, Action, Predicate

--- EventsAndDelegates/DelegateWorkerSupervisorExample.cs
+++ EventsAndDelegates/DelegateWorkerSupervisorExample.cs
@@ -3,9 +3,8 @@ using System.Threading;
 
 
 public delegate void TaskStarted(Worker worker, DateTime dt);
-public delegate void TaskProgressUpdate(Worker worker, byte progress);
+public delegate void TaskProgressUpdate(Worker worker, float progress);
 public delegate int TaskCompleted(Worker worker, int cost);
-
 
 
 class DelegateWorkerSupervisorExample
@@ -17,7 +16,7 @@ class DelegateWorkerSupervisorExample
         Worker worker1 = new Worker(1, "Generate Report");
         Supervisor supervisor = new Supervisor();
 
-        worker1.ExecuteJob(supervisor.WorkerTaskStarted, supervisor.WorkerTaskProgressUpdate, supervisor.WorkerTaskCompleted);
+        worker1.ExecuteJob(supervisor.OnTaskStarted, supervisor.OnTaskProgressUpdate, supervisor.OnTaskCompleted);
     }
 }
 
@@ -25,28 +24,21 @@ class DelegateWorkerSupervisorExample
 
 public class Supervisor
 {
-    public void WorkerTaskStarted(Worker worker, DateTime dt)
+    public void OnTaskStarted(Worker worker, DateTime dt)
     {
         Console.WriteLine($"Supervisor: Worker({worker.WorkerId}) Started doing task({worker.Job}) at {dt.ToString()}");
     }
 
-    public void WorkerTaskProgressUpdate(Worker worker, byte progress)
+    public void OnTaskProgressUpdate(Worker worker, float progress)
     {
         Console.WriteLine($"Supervisor: Task Update from Worker({worker.WorkerId}). Task Progress: {progress}");
     }
 
-    public int WorkerTaskCompleted(Worker worker, int cost)
+    public int OnTaskCompleted(Worker worker, int cost)
     {
         Console.WriteLine("Supervisor: Worker Task completed");
+        return worker.WorkerId == 1 ? cost * 3 : cost * 2;
 
-        var rewardsPoints = cost * 2;
-
-        if (worker.WorkerId == 1)
-        {
-            return rewardsPoints + 100;
-        }
-
-        return rewardsPoints;
     }
 }
 
@@ -66,20 +58,22 @@ public class Worker
         Console.WriteLine("Worker: Starting Task.");
         taskStartedCallback(this, DateTime.Now);
 
-        var cost = 0;
+        var totalCost = 0;
         var random = new Random();
 
-        for (int i = 1; i <= 10; i++)
+        for (int i = 1; i <= 5; i++)
         {
-
             Thread.Sleep(300);
-            cost += random.Next(1, 50);
+
+            totalCost += random.Next(1, 50);
+            var progress = ((float)i / 5) * 100;
             Console.WriteLine("Worker: Updating about task progress.");
-            taskProgressUpdateCallback(this, ((byte)(i * 10)));
+
+            taskProgressUpdateCallback(this, progress);
         }
 
         Console.WriteLine("Worker: Updating about task completion.");
-        var rewardsPoints = taskCompletedCallback(this, cost);
+        var rewardsPoints = taskCompletedCallback(this, totalCost);
 
         Console.WriteLine($"Worker: Task Completed, Reward Points: {rewardsPoints}");
     }
