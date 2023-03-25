997bb6dfe5103fc363d238c5b9907a23dbfe6926
Basit <devxbasit@gmail.com>
2023-03-25 23:31:40 +0530

    typo fix

--- LearnCSharpProgramming_MariusBancila/Chapter_1_BuildingBlocksOfC#/Yield_Basic.cs
+++ LearnCSharpProgramming_MariusBancila/Chapter_1_BuildingBlocksOfC#/Yield_Basic.cs
@@ -3,23 +3,27 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 
-class Yeild_Basic
+class Yield_Basic
 {
-    public static void Main(){
+    public static void Main()
+    {
 
         //A yield return statement returns an item as soon as it is available.
 
-        foreach (var number in GetNumbers().Take(5)){
+        foreach (var number in GetNumbers().Take(5))
+        {
             Console.WriteLine($"Consumed: {number}");
         }
     }
 
-    public static IEnumerable<int> GetNumbers(){
-        
+    public static IEnumerable<int> GetNumbers()
+    {
+
         // var list = new List<int>();
 
-        for (int i = 0; i < 10; i++) {
-            
+        for (int i = 0; i < 10; i++)
+        {
+
             Thread.Sleep(1000);
             Console.WriteLine($"Produced: {i}");
             yield return i;
@@ -27,5 +31,5 @@ class Yeild_Basic
             //list.Add(i);
         }
         //return list;
-    }    
+    }
 }
